7a2cf2aea5c069ac2b82e5145334532a1bdffe8d
Mahesh Ranpise <mahesh8276@gmail.com>
2020-11-08 23:10:48 +0530

    Task 2

---
+++ Week 1/Task 2/scripts.js
@@ -0,0 +1,22 @@
+//1. crete a request variable 
+
+var request = new XMLHttpRequest();
+
+//2. open a connection 
+
+request.open('GET', 'https://restcountries.eu/rest/v2/all', true)
+
+//3. send the request 
+
+request.send();
+
+//4. load the response 
+
+request.onload = function () {
+    var data = JSON.parse(this.response);
+    {
+        for(i=0;i<data.length;i++){
+            console.log(data[i].flag);
+        }
+    }
+}
